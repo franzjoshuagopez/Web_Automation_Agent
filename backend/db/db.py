491e3c82cbbc91acc3875d76cd124df01c86c977
franzgopezEY <29223903+franzjoshuagopez@users.noreply.github.com>
2025-11-01 16:18:05 +0800

    update

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,7 +1,22 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite+aiosqlite:///./backend/memory/chatbot.db"
+ENV = os.getenv("ENV", "development")
+
+DATABASE_URL =""
+
+load_dotenv()
+
+if ENV == "production":
+    DATABASE_URL = os.getenv("PRODUCTION_BASE_URL")
+    if DATABASE_URL is None:
+        DATABASE_URL = "sqlite+aiosqlite:///./backend/memory/chatbot.db"
+elif ENV == "development":
+    DATABASE_URL = os.getenv("DEVELOPMENT_BASE_URL")
+    if DATABASE_URL is None:
+        DATABASE_URL = "sqlite+aiosqlite:///./backend/memory/chatbot.db"
 
 engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 AsyncSessionLocal = async_sessionmaker(
